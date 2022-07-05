d7cfa7c1127867da9062dbaae4fb2fd7a435cf03
owuordove@gmail.com <owuordove@gmail.com>
2022-07-05 17:09:21 +0300

    0. A library is not a luxury but one of the necessities of life

---
+++ 0x18-dynamic_libraries/0-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code for Alx-MAIN_H School students.
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+int main(void)
+{
+    printf("%d\n", _strlen("MAIN_H"));
+    return (EXIT_SUCCESS);
+}
