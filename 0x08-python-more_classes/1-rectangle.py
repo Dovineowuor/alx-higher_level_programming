f148a5d5f848668392cfcf9a2676c317afc36d53
Dovine Owuor <70941806+Dovineowuor@users.noreply.github.com>
2022-07-26 09:51:56 +0300

    1. Real definition of a rectangle

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,94 @@
+#!/usr/bin/python3
+""" 1-rectangle
+
+    Class Rectangle
+
+    Raises:
+        TypeError: height must be an integer
+        ValueError: height must be >= 0
+        TypeError: width must be an integer
+        ValueError: width must be >= 0
+"""
+
+
+class Rectangle():
+    """Rectangle Class
+
+    Class with Width and height properties
+    """
+
+    def __init__(self, width=0, height=0):
+        """__init__ constructor
+
+        Contructor of Rectangle class
+
+        Args:
+            width (int, optional): Width by Rectangle. Defaults to 0.
+            height (int, optional): Height by Rectangle. Defaults to 0.
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def height(self):
+        """height Getter
+
+        Get height value
+
+        Returns:
+            int: height value
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """height Setter
+
+        Set height value
+
+        Args:
+            value (int): value to set
+
+        Raises:
+            TypeError: height must be an integer
+            ValueError: height must be >= 0
+        """
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+
+        if value < 0:
+            raise ValueError("height must be >= 0")
+
+        self.__height = value
+
+    @property
+    def width(self):
+        """width Getter
+
+        Get width value
+
+        Returns:
+            int: width value
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """width Setter
+
+        set width value
+
+        Args:
+            value (int): value to set
+
+        Raises:
+            TypeError: width must be an integer
+            ValueError: width must be >= 0
+        """
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+
+        if value < 0:
+            raise ValueError("width must be >= 0")
+
+        self.__width = value
