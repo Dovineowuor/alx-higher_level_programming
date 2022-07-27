edde1773ed90bd64216349958de40bd540302939
Dovine Owuor <70941806+Dovineowuor@users.noreply.github.com>
2022-07-27 05:25:37 +0300

    30. Low memory cost

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" 101-locked_class
+
+    LockedClass
+"""
+
+
+class LockedClass():
+    """LockedClass
+
+    attribute "first_name"
+    """
+    __slots__ = ["first_name"]
