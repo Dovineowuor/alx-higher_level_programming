8c4a1e6700bfb66c13da68bed1ca77a580d4fe9f
Dovine Owuor <70941806+Dovineowuor@users.noreply.github.com>
2022-10-14 16:00:17 +0300

    Fix bug

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,28 +1,27 @@
 #!/usr/bin/python3
+"""A script that adds object “Louisiana”
+to the database hbtn_0e_6_us"""
 
 
-import sqlalchemy as db
-from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
 from sys import argv
 
 
 if __name__ == "__main__":
-    engine = db.create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        argv[1], argv[2], argv[3]), pool_pre_ping=True)
 
+    import sys
+    from model_state import Base, State
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import Session
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2],
+                                   sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
-    Session = sessionmaker(bind=engine)
-    session = Session()
-
-    new = State(name='Louisiana')
-
-    new = State(name='Louisiana')
-    session.add(new)
+    session = Session(engine)
+    new_st = State(name='Louisiana')
+    session.add(new_st)
+    new_state = session.query(State).filter(State.name == 'Louisiana').first()
     session.commit()
+    print('{}'.format(new_state.id))
     session.close()
-
-    Murica = session.query(State).filter(State.name == 'Louisiana').one()
-
-    print(Murica.id)
