6bf91b47edb2f800beba6ff45c62a0ec36d08274
Dovine Owuor <70941806+Dovineowuor@users.noreply.github.com>
2022-10-14 15:52:19 +0300

    fix Bug

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python3
+"""script that takes argument and displays
+values in the states"""
 
+if __name__ == '__main__':
 
-import MySQLdb
-from sys import argv
+    import MySQLdb
+    import sys
 
-if __name__ == "__main__":
-    database = MySQLdb.connect(user=argv[1],
-                               passwd=argv[2],
-                               db=argv[3])
-    curs = database.cursor()
-    curs.execute("SELECT * FROM states WHERE name\
-                  LIKE %s ORDER BY states.id\
-                  ASC", [argv[4]])
-    rows = curs.fetchall()
+    db = MySQLdb.connect(host='localhost', port=3306,
+                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name=%s\
+                ORDER BY states.id ASC", (sys.argv[4],))
+    rows = cur.fetchall()
     for row in rows:
         print(row)
-    curs.close()
-    database.close()
