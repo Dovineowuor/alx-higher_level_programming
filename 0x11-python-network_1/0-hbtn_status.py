1d6d054a0859526523e2aea4a12c88fb27e67cf5
root <Dovineowuor>
2022-10-27 18:05:12 -0700

    update

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,17 +1,24 @@
 #!/usr/bin/python3
+"""A script that
+- fetches https://alx-intranet.hbtn.io/status.
+- uses urlib package
+"""
+if __name__ == '__main__':
+        import urllib.request
+            with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
+                        content = res.read()
+                                print("Body response:")
+                                        print("\t- type: {}".format(type(content)))
+                                                print("\t- content: {}".format(content))
+                                                        print("\t- utf8 content: {}".format(content.decode('utf-8')))#!/usr/bin/python3
 """Fetches the URL: https://intranet.hbtn.io/status
 """
-
 from urllib.request import Request, urlopen
-
-
 if __name__ == "__main__":
     req = Request('https://intranet.hbtn.io/status')
-
     with urlopen(req) as res:
         content = res.read()
         utf8_content = content.decode('utf-8')
-
         print('Body response:')
         print('\t- type: {_type}'.format(_type=type(content)))
         print('\t- content: {_content}'.format(_content=content))
