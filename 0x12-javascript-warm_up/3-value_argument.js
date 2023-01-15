c9d94e58697d6abb0a1b627ee2ebb24519c4e44f
Dovineowuor <owuordove@gmail.com>
2023-01-15 04:31:38 +0300

    fix bugs

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
 
-const process = require('process');
-if (process.argv[2] === undefined) {
-  console.log('No argument');
+const argNum = process.argv;
+
+if (argNum[2]) {
+  console.log(argNum[2]);
 } else {
-  console.log(process.argv[2]);
+  console.log('No argument');
 }
