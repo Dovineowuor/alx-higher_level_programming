e177b45714b2c7af03ed79277283c73c58ad8727
Dovineowuor <owuordove@gmail.com>
2023-01-15 04:32:32 +0300

    fix bugs

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,9 +1,27 @@
 #!/usr/bin/node
 
-const process = require('process');
 const args = process.argv;
-if (args.length < 4) {
-  console.log(0);
+let biggest = process.argv[2];
+const firstArg = process.argv[0];
+const secondArg = process.argv[1];
+
+let i;
+
+if (!(firstArg && secondArg && (args.length < 4))) {
+  for (let i = 2; i < args.length; i++) {
+    if (biggest < process.argv[i]) {
+      biggest = process.argv[i];
+    }
+  }
+  let secondBiggest = process.argv[2];
+  for (let j = 2; j < args.length; j++) {
+    if (process.argv[j] !== biggest && i !== j) {
+      if (process.argv[j] > secondBiggest) {
+        secondBiggest = process.argv[j];
+      }
+    }
+  }
+  console.log(secondBiggest);
 } else {
-  console.log(args.sort().reverse()[1]);
+  console.log(0);
 }
