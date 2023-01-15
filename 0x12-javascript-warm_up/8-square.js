c5a5a35588b9911768887ca3575952240d3a650c
Dovineowuor <owuordove@gmail.com>
2023-01-15 04:32:15 +0300

    fix bugs

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,11 +1,15 @@
 #!/usr/bin/node
 
-const process = require('process');
-const arg = process.argv[2];
-if (isNaN(arg)) {
+const argNum = process.argv[2];
+
+if (isNaN(argNum)) {
   console.log('Missing size');
 } else {
-  for (let index = 0; index < arg; index++) {
-    console.log('X'.repeat(arg));
+  for (let i = 0; i < argNum; i++) {
+    let str = '';
+    for (let j = 0; j < argNum; j++) {
+      str += 'X';
+    }
+    console.log(str);
   }
 }
